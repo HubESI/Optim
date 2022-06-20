c46899fe5345b6ca7265824d7e2e5d445609c2b3
nassimb0u <in_boulechfar@esi.dz>
2022-06-20 23:33:13 +0100

    add more adj matrix features

--- views/adjacency_matrix.py
+++ views/adjacency_matrix.py
@@ -1,4 +1,5 @@
-from tkinter import CENTER, ttk
+from random import randint
+from tkinter import CENTER, END, ttk
 
 from .config import base_padding
 
@@ -14,14 +15,20 @@ class AdjacencyMatrix(ttk.Frame):
         self.error_label = ttk.Label(self, foreground="red")
         self.error_label.grid(row=n_max + 1, column=0, columnspan=n_max, pady=5)
 
-        def validator_wrapper(v):
+        def validator_wrapper(v, i, j):
             if self._cell_validator(v):
+                i, j = int(i), int(j)
+                if i != j:
+                    self.cells[j][i].config(validate="none")
+                    self.cells[j][i].delete(0, "end")
+                    self.cells[j][i].insert(0, v)
+                    self.cells[j][i].config(validate="key")
                 self.error_label["text"] = ""
                 return True
             self.error_label["text"] = "Veuillez entrer 0 ou 1"
             return False
 
-        vcmd = (self.register(validator_wrapper), "%P")
+        vcmd = self.register(validator_wrapper)
         for i in range(n_max):
             for j in range(n_max):
                 self.cells[i][j] = ttk.Entry(
@@ -29,8 +36,11 @@ class AdjacencyMatrix(ttk.Frame):
                     justify=CENTER,
                     width=5,
                     validate="key",
-                    validatecommand=vcmd,
+                    validatecommand=(vcmd, "%P", i, j),
                 )
+                if i == j:
+                    self.cells[i][j].insert(0, "0")
+                    self.cells[i][j].config(state="disabled")
                 self.cells[i][j].grid(row=i + 1, column=j)
 
     @staticmethod
@@ -49,3 +59,38 @@ class AdjacencyMatrix(ttk.Frame):
                 if v:
                     m[i][j] = int(v)
         return m
+
+    def set_n(self, new_n):
+        if new_n > self.n_max:
+            new_n = self.n_max
+        if new_n <= self.n:
+            for i in range(new_n, self.n):
+                for j in range(0, self.n):
+                    if i != j:
+                        self.cells[i][j].delete(0, "end")
+                        self.cells[i][j].config(state="disabled")
+                        self.cells[j][i].delete(0, "end")
+                        self.cells[j][i].config(state="disabled")
+                    else:
+                        self.cells[i][i].config(state="normal")
+                        self.cells[i][i].delete(0, "end")
+                        self.cells[i][i].config(state="disabled")
+        else:
+            for i in range(self.n, new_n):
+                for j in range(0, new_n):
+                    if i != j:
+                        self.cells[i][j].config(state="normal")
+                        self.cells[j][i].config(state="normal")
+                    else:
+                        self.cells[i][i].config(state="normal")
+                        self.cells[i][i].insert(0, "0")
+                        self.cells[i][i].config(state="disabled")
+        self.n = new_n
+
+    def fill_random(self):
+        for i in range(0, self.n):
+            for j in range(i, self.n):
+                if i != j:
+                    self.cells[i][j].delete(0, "end")
+                    r = randint(0, 1)
+                    self.cells[i][j].insert(0, str(r))
